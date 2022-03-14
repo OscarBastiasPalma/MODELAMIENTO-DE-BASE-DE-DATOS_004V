44c2208ba1983206f73a7202dcca83c1086dcc51
oscar bastias <oscarbastias@oscars-MacBook-Pro.local>
2022-03-14 19:31:21 -0300

    first commit

---
+++ 07-03-2022.sql
@@ -0,0 +1,9 @@
+unidad 1: construir modelos conceptuales simples:
+unidad 2: modelo conceptual complejo modelos conceptuales avanzados y normalizados
+unidad 3: modelo relacional y esta normalizado e implementar en una DB.
+
+pruebas: 
+lunes 4 de abril 25%
+lunes 16 de mayo 40%
+lunes 27 de junio 35%
+
