f85e01a5f428cf1def042bc8ff007ba3ad0ed134
oscar bastias <oscarbastias@oscars-MacBook-Pro.local>
2022-03-14 22:20:05 -0300

    clase 14-03-2022

---
+++ 14-03-2022.sql
@@ -0,0 +1,26 @@
+constraing se vera en la unidad 3.
+
+diseño conceptual: esquema abastracto que representa los requisitos del negocio, independientemente del modelo de base de datos a usar.
+diseño logico: define la estructura logica de organizacion de los datos en funcion del modelo base de datos a usar.
+diseño fisico : define las especificaciones fisica del implementacion para el SGBD se haya elegido.
+
+1.- narrativa y vistas de usuario herramientas para adquirir lo que el cliente necesita, nunca ASUMIR!, != yo pensaba que era asi.
+
+Entidades: 
+
+La entidad va a ser uno de los obejetos utilizados en nuestro contexto (NEGOCIO)
+
+Atributos: 
+
+son caracteristicas que definen a una entidad. el atributo es general.
+deben ser importantes en el contexto(NEGOCIO)
+
+
+¿Como reconocer una entidad?
+
+el 95% las entidad son sustantivos (Objetos, personas, etc)
+
+el objeto tiene atributos propios? si es asi, estamos en presencia de una entidad.
+
+entidades fuertes: no necesita para poder existir. ej: no depende de otra  ej, productos
+entidades caracteristicas o debil: necesita de otra para poder existir.
